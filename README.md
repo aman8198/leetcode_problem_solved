a77526cf1b4b0fb7d4f67b212d4525a2c7bd4251
Aman kumar Jha <jhaaman8198@gmail.com>
2024-06-24 12:07:41 +0530

    reverse Integer Problem Solved

--- README.md
+++ README.md
@@ -2,14 +2,20 @@
 solving leetcode problems for job placement 
 
 Solved Two Sum Problem 
+Link: https://leetcode.com/problems/two-sum/
 
 Solved Longest Substring Without Repeating Characters
+Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
 Solved Median of two sorted array
+Link: https://leetcode.com/problems/median-of-two-sorted-arrays/
 
 Solved  Longest Palindromic Substring
+Link: https://leetcode.com/problems/longest-palindromic-substring/
 
 Solved ZigZag Conversion Problem 
+Link: https://leetcode.com/problems/zigzag-conversion/description/
 
-
+Solved Reverse Integer Problem 
+Link: https://leetcode.com/problems/reverse-integer/
 
---
+++ ReverseInteger/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public int Reverse(int x)
+    {
+
+        int reversed = 0;
+        while (x != 0)
+        {
+            int pop = x % 10;
+            x /= 10;
+            //check for overflow before updating reversed 
+            if (reversed > int.MaxValue / 10 || (reversed == int.MaxValue / 10 && pop > 7)) return 0;
+            if (reversed < int.MinValue / 10 || (reversed == int.MinValue / 10 && pop < -8)) return 0;
+            reversed = reversed * 10 + pop;
+        }
+        return reversed;
+
+    }
+
+    public static void Main()
+    {
+        Solution s = new Solution();
+        var result = s.Reverse (10);
+        Console.WriteLine (result);
+    }
+}
